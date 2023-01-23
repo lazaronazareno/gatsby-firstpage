3f3741a81c2e2ab59bba495d55c96cf151aba66c
lazaronazareno <lazaronazareno@gmail.com>
2023-01-23 15:40:43 -0300

    create the pages

--- src/pages/us.js
+++ src/pages/us.js
@@ -1,36 +1,20 @@
-import * as React from 'react'
-import ContentHome from '../components/contentHome'
-import CoursePreview from '../components/coursePreview'
-import GirlImage from '../components/girlImage'
+import React from 'react'
+import ContentUs from '../components/contentUs'
 import Layout from '../components/layout'
-import useCourse from '../hooks/useCourse'
-import * as style from '../styles/global.module.css'
 
-const IndexPage = () => {
-  const courses = useCourse()
+const Us = () => {
   return (
     <Layout>
-      <GirlImage />
-
-      <ContentHome />
-
-      <h2 className={style.title}>Nuestros cursos</h2>
-
-      <ul className={style.cmsCourse_list}>
-        {courses.map(course => (
-          <CoursePreview key={course.id} course={course} />
-        ))}
-      </ul>
-
+      <ContentUs />
     </Layout>
   )
 }
 
-export default IndexPage
+export default Us
 
 export const Head = () => (
   <>
-    <title>May Help?</title>
+    <title>Nosotros - May Help?</title>
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css' integrity='sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==' crossOrigin='anonymous' referrerPolicy='no-referrer' />
     <link rel='preconnect' href='https://fonts.googleapis.com' />
     <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
